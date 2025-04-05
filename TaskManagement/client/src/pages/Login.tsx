54201abfd3edaa1017352233848bf87834617aab
Bibek Tamang <bibek7here@gmail.com>
2025-04-05 19:44:57 +0545

    updated client route and design

--- TaskManagement/client/src/pages/Login.tsx
+++ TaskManagement/client/src/pages/Login.tsx
@@ -42,97 +42,109 @@ const Login = () => {
 	}, []);
 
 	return (
-		<section className="w-full min-h-screen h-full p-8">
-			<div className="lg:w-[50%] w-full h-full flex flex-col items-center justify-between">
-				<Link
-					to={`/`}
-					className="text-gradient">
-					<h1 className="text-4xl text-center mb-16">Donezo</h1>
-				</Link>
-				<div className="lg:mx-20 flex flex-col items-center justify-between">
-					<h2 className="text-4xl my-4 text-center">Welcome Back</h2>
-					<div className="flex flex-col">
-						<p className="text-sm font-extralight text-center text-gray-400">
-							Enter you email and password to access your account
-						</p>
-						<form
-							action=""
-							onSubmit={handleLogin}>
-							<div className="flex flex-col flex-1 my-4">
-								<label htmlFor="email">Email</label>
-								<Input
-									id="email"
-									placeholder="Enter your email"
-									defaultValue={email}
-									onChange={(e) => setEmail(e.target.value)}
-									className="my-2 outline-none border-none bg-gray-800 py-6"
-								/>
-							</div>
-							<div className="flex flex-col my-4">
-								<label htmlFor="password">Password</label>
-								<Input
-									id="password"
-									placeholder="Enter your password"
-									type="password"
-									onChange={(e) => setPassword(e.target.value)}
-									className="my-2 outline-none border-none bg-gray-800 py-6"
-								/>
-							</div>
-							<div className="flex items-center justify-between">
-								<div
-									onClick={() => setIsRemember((prev) => !prev)}
-									className="flex items-center space-x-2">
-									<input
-										checked={isRemember}
-										type="checkbox"
-										onChange={() => setIsRemember((prev) => !prev)}
+		<section className="w-full max-h-screen h-screen overflow-hidden">
+			<div className="flex h-full">
+				<div className="lg:w-[50%] w-full py-12 pb-12  h-full flex flex-col items-center justify-center">
+					<div className="lg:mx-20 flex flex-col items-center justify-between">
+						<h2 className="text-4xl my-4 text-center">Welcome Back</h2>
+						<div className="flex flex-col">
+							<p className="text-sm font-extralight text-center text-gray-400">
+								Enter you email and password to access your account
+							</p>
+							<form
+								action=""
+								onSubmit={handleLogin}
+							>
+								<div className="flex flex-col flex-1 my-4">
+									<label htmlFor="email">Email</label>
+									<Input
+										id="email"
+										placeholder="Enter your email"
+										defaultValue={email}
+										onChange={(e) => setEmail(e.target.value)}
+										className="my-2 outline-none border-none bg-gray-800 py-6"
 									/>
-									<label
-										htmlFor="terms"
-										className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-										Remember me
-									</label>
 								</div>
-								<Link
-									className="text-sm"
-									to={`/forgot-password`}>
-									Forgot Password?
-								</Link>
+								<div className="flex flex-col my-4">
+									<label htmlFor="password">Password</label>
+									<Input
+										id="password"
+										placeholder="Enter your password"
+										type="password"
+										onChange={(e) => setPassword(e.target.value)}
+										className="my-2 outline-none border-none bg-gray-800 py-6"
+									/>
+								</div>
+								<div className="flex items-center justify-between">
+									<div
+										onClick={() => setIsRemember((prev) => !prev)}
+										className="flex items-center space-x-2"
+									>
+										<input
+											checked={isRemember}
+											type="checkbox"
+											onChange={() => setIsRemember((prev) => !prev)}
+										/>
+										<label
+											htmlFor="terms"
+											className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+										>
+											Remember me
+										</label>
+									</div>
+									<Link
+										className="text-sm"
+										to={`/forgot-password`}
+									>
+										Forgot Password?
+									</Link>
+								</div>
+								<Button
+									className={cn(
+										"w-full mt-4 py-5",
+										isLoading ? "bg-red-600" : ""
+									)}
+									type="submit"
+								>
+									Sign In
+								</Button>
+							</form>
+							<div>
+								<Button
+									className="w-full py-5 text-white hover:bg-slate-700 outline-none bg-gray-900 border-2 border-slate-500 my-4"
+									onClick={handleSignInWithGoogle}
+								>
+									<img
+										width={20}
+										height={20}
+										src="/logos/google.svg"
+										alt="Google logo"
+									/>
+									Sign in with Google
+								</Button>
 							</div>
-							<Button
-								className={cn(
-									"w-full mt-12 py-5",
-									isLoading ? "bg-red-600" : ""
-								)}
-								type="submit">
-								Sign In
-							</Button>
-						</form>
-						<div>
-							<Button
-								className="w-full py-5 text-white hover:bg-slate-700 outline-none bg-gray-900 border-2 border-slate-500 my-4"
-								onClick={handleSignInWithGoogle}>
-								<img
-									width={20}
-									height={20}
-									src="/logos/google.svg"
-									alt="Google logo"
-								/>
-								Sign in with Google
-							</Button>
 						</div>
 					</div>
+					<div className="flex">
+						<p className="text-sm text-gray-500">
+							Don't have an account?
+							<Link
+								className="text-white font-normal"
+								to={`/signup`}
+							>
+								{" "}
+								Sign Up
+							</Link>
+						</p>
+					</div>
 				</div>
-				<div className="flex">
-					<p className="text-sm text-gray-500">
-						Don't have an account?
-						<Link
-							className="text-white font-normal"
-							to={`/signup`}>
-							{" "}
-							Sign Up
-						</Link>
-					</p>
+				<div className="flex-1 h-full">
+					<img
+						src="/images/taskPerson.png"
+						className="w-full h-full object-contain "
+						alt="task clock image"
+						loading="lazy"
+					/>
 				</div>
 			</div>
 		</section>
--- TaskManagement/client/src/pages/Signup.tsx
+++ TaskManagement/client/src/pages/Signup.tsx
@@ -46,97 +46,110 @@ const Signup = () => {
 	};
 
 	return (
-		<section className="w-full max-h-screen h-screen p-8">
-			<div className="lg:w-[50%] w-full h-full flex flex-col items-center justify-center">
-				<div className="lg:mx-20 h-full flex flex-col items-center justify-center">
-					<h2 className="text-4xl my-4 text-center">Create an account</h2>
-					<div className="flex flex-col">
-						<p className="text-sm font-extralight mb-6  text-center text-gray-400">
-							Embrace productivity and efficiency by optimizing your tasks
-						</p>
-						<FormProvider {...form}>
-							<form
-								onSubmit={form.handleSubmit(handleRegister)}
-								className="flex flex-col gap-4">
-								<FormField
-									control={form.control}
-									name="email"
-									rules={{ required: "Email is required" }}
-									render={({ field, fieldState }) => (
-										<FormItem>
-											<FormLabel htmlFor="email">Email</FormLabel>
-											<FormControl>
-												<Input
-													id="email"
-													placeholder="Enter your email"
-													className="my-2 outline-none border-none bg-gray-800 py-6"
-													{...field}
-												/>
-											</FormControl>
-											{form.formState.errors.email && (
-												<span className="text-red-500 text-sm">
-													{fieldState.error?.message}
-												</span>
-											)}
-										</FormItem>
-									)}
-								/>
-								<FormField
-									control={form.control}
-									name="password"
-									rules={{ required: "Password is required" }}
-									render={({ field, fieldState }) => (
-										<FormItem>
-											<FormLabel htmlFor="password">password</FormLabel>
-											<FormControl>
-												<Input
-													id="password"
-													placeholder="Enter your password"
-													className="my-2 outline-none border-none bg-gray-800 py-6"
-													type="password"
-													{...field}
-												/>
-											</FormControl>
-											{form.formState.errors.password && (
-												<span className="text-red-500 text-sm">
-													{fieldState.error?.message}
-												</span>
-											)}
-										</FormItem>
-									)}
-								/>
-								<Button
-									type="submit"
-									className={cn(
-										"w-full mt-4 py-5",
-										isLoading ? "bg-red-600" : ""
-									)}>
-									Get Started
+		<section className="w-full max-h-screen h-screen overflow-hidden px-4">
+			<div className="flex h-full">
+				<div className="lg:w-[50%] w-full h-full flex flex-col items-center justify-center">
+					<div className="lg:mx-20 h-full flex flex-col items-center justify-center">
+						<h2 className="text-4xl my-4 text-center">Create an account</h2>
+						<div className="flex flex-col">
+							<p className="text-sm font-extralight mb-6  text-center text-gray-400">
+								Embrace productivity and efficiency by optimizing your tasks
+							</p>
+							<FormProvider {...form}>
+								<form
+									onSubmit={form.handleSubmit(handleRegister)}
+									className="flex flex-col gap-4"
+								>
+									<FormField
+										control={form.control}
+										name="email"
+										rules={{ required: "Email is required" }}
+										render={({ field, fieldState }) => (
+											<FormItem>
+												<FormLabel htmlFor="email">Email</FormLabel>
+												<FormControl>
+													<Input
+														id="email"
+														placeholder="Enter your email"
+														className="my-2 outline-none border-none bg-gray-800 py-6"
+														{...field}
+													/>
+												</FormControl>
+												{form.formState.errors.email && (
+													<span className="text-red-500 text-sm">
+														{fieldState.error?.message}
+													</span>
+												)}
+											</FormItem>
+										)}
+									/>
+									<FormField
+										control={form.control}
+										name="password"
+										rules={{ required: "Password is required" }}
+										render={({ field, fieldState }) => (
+											<FormItem>
+												<FormLabel htmlFor="password">password</FormLabel>
+												<FormControl>
+													<Input
+														id="password"
+														placeholder="Enter your password"
+														className="my-2 outline-none border-none bg-gray-800 py-6"
+														type="password"
+														{...field}
+													/>
+												</FormControl>
+												{form.formState.errors.password && (
+													<span className="text-red-500 text-sm">
+														{fieldState.error?.message}
+													</span>
+												)}
+											</FormItem>
+										)}
+									/>
+									<Button
+										type="submit"
+										className={cn(
+											"w-full mt-4 py-5",
+											isLoading ? "bg-red-600" : ""
+										)}
+									>
+										Get Started
+									</Button>
+								</form>
+							</FormProvider>
+							<div>
+								<Button className="w-full py-5 bg-gray-900 text-white border-2 border-slate-500 my-4 hover:bg-slate-700">
+									<img
+										width={20}
+										height={20}
+										src="/logos/google.svg"
+										alt="Google logo"
+									/>
+									Sign up with Google
 								</Button>
-							</form>
-						</FormProvider>
-						<div>
-							<Button className="w-full py-5 bg-gray-900 text-white border-2 border-slate-500 my-4 hover:bg-slate-700">
-								<img
-									width={20}
-									height={20}
-									src="/logos/google.svg"
-									alt="Google logo"
-								/>
-								Sign up with Google
-							</Button>
+							</div>
 						</div>
-					</div>
 
-					<p className="text-sm text-gray-500">
-						Already have an account?
-						<Link
-							className="text-white font-normal"
-							to={`/login`}>
-							{" "}
-							Login
-						</Link>
-					</p>
+						<p className="text-sm text-gray-500">
+							Already have an account?
+							<Link
+								className="text-white font-normal"
+								to={`/login`}
+							>
+								{" "}
+								Login
+							</Link>
+						</p>
+					</div>
+				</div>
+				<div className="flex-1 h-full lg:block hidden">
+					<img
+						src="/images/taskPerson.png"
+						alt="task person image"
+						className="w-full h-full object-contain"
+						loading="lazy"
+					/>
 				</div>
 			</div>
 		</section>
